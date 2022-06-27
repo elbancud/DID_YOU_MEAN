d050690dcff9d495bac20f8d1b9c0c094c37bacd
emarson <elbancud@gmail.com>
2022-06-27 10:54:39 +0800

    chore: form scaffold structured

--- did_you_mean/src/App.tsx
+++ did_you_mean/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import MainPage from "./pages/client/main-page";
 
 function App() {
-	return <div className="App">hejhejf</div>;
+	return (
+		<div className="App bg-slate-900 text-white h-screen p-5">
+			<MainPage />
+		</div>
+	);
 }
 
 export default App;
---
+++ did_you_mean/src/pages/client/main-page.tsx
@@ -0,0 +1,49 @@
+import react from "react";
+function MainPage() {
+	return (
+		<div>
+			<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+				<div className="max-w-md w-full space-y-8">
+					<div>
+						{/* <img
+							className="mx-auto h-12 w-auto"
+							src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+							alt="Workflow"
+						/> */}
+						<h2 className="mt-6 text-center text-xl font-extrabold ">
+							Enter a phrase?
+						</h2>
+					</div>
+					<form className="mt-8 space-y-6" action="#" method="GET">
+						<div className="rounded-md shadow-sm -space-y-px">
+							<div>
+								<label htmlFor="email-address" className="sr-only">
+									Phrase
+								</label>
+								<input
+									id="phrase"
+									name="phrase"
+									type="text"
+									autoComplete="phrase"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Input"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<button
+								type="submit"
+								className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+							>
+								input
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default MainPage;
